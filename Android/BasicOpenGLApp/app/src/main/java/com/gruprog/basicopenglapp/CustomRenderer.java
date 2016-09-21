4886b25b01f178a0164330519dc5b9a78a31ab64
Jai Srikar <gruprog@gmail.com>
2016-09-20 22:02:40 -0400

    custom renderer

---
+++ Android/BasicOpenGLApp/app/src/main/java/com/gruprog/basicopenglapp/CustomRenderer.java
@@ -0,0 +1,24 @@
+package com.gruprog.basicopenglapp;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class CustomRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        GLES20.glClearColor(0.2f, 0.5f, 1.0f, 0.4f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+}
--- Android/BasicOpenGLApp/app/src/main/java/com/gruprog/basicopenglapp/MainActivity.java
+++ Android/BasicOpenGLApp/app/src/main/java/com/gruprog/basicopenglapp/MainActivity.java
@@ -1,13 +1,58 @@
 package com.gruprog.basicopenglapp;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        glSurfaceView = new GLSurfaceView(this);
+
+        final boolean es2Supported = isES2Supported();
+
+        if(es2Supported) {
+            glSurfaceView.setEGLContextClientVersion(2);
+            glSurfaceView.setRenderer(new CustomRenderer());
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "OpenGL ES 2.0 is not supported on this device", Toast.LENGTH_LONG).show();
+        }
+
+        setContentView(glSurfaceView);
+    }
+
+    private boolean isES2Supported() {
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+
+        return configurationInfo.reqGlEsVersion >= 0x20000;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(rendererSet) {
+            glSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(rendererSet) {
+            glSurfaceView.onPause();
+        }
     }
 }
