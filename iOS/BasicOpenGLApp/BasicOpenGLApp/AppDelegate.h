49b04b99920227cc2624f42ea8f855c4aa2d041c
Jai Srikar <gruprog@gmail.com>
2016-09-23 20:32:29 -0400

    a basic opengl app

---
+++ iOS/BasicOpenGLApp/BasicOpenGLApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicOpenGLApp
+//
+//  Created by droid on 2016-09-23.
+//  Copyright (c) 2016 droid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/BasicOpenGLApp/BasicOpenGLApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BasicOpenGLApp
+//
+//  Created by droid on 2016-09-23.
+//  Copyright (c) 2016 droid. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
